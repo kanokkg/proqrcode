13e78f294c0b8b7c36ff98666fb55b1de368775c
kanokkg <30363022+kanokkg@users.noreply.github.com>
2025-03-29 17:56:45 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@ const form = document.forms['contact-form']
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! Form is submitted" ))
+  .then(response => alert("ขอบคุณสำหรับการอุดหนุนครับ !!!" ))
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
 })
